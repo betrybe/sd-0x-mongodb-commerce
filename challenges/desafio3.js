545e33fe0a6019f9f7ebfe474e1a7fd6d10c1c7a
Rogério Munhoz <roz@rjmunhoz.me>
2020-10-29 19:13:29 -0300

    Testa avaliação automática

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,3 +1,7 @@
-db.produtos.update({}, { $set: { criadoPor: 'Ronald McDonald' } });
+db.produtos.update(
+  {},
+  { $set: { criadoPor: 'Ronald McDonald' } },
+  { multi: true },
+);
 
 db.produtos.find({}, { name: 1, criadoPor: 1 });
